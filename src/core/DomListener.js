bc71643e0d4a6ee41f8f35d559d3f445840b1674
Alexandr Abrosimov <mystudyjs11@gmail.com>
2020-08-06 23:17:46 +0700

    add Listeners. To be continued....

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,10 +1,33 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable linebreak-style */
+
+import {capitalize} from './utils';
+
+
 export class DomListener {
-  constructor($root) {
+  constructor($root, listeners = []) {
     if (!$root) {
       throw new Error(`No $root provided for DomListener!`);
     }
-    this.root = $root;
-    console.log('this.root: ', this.root);
+    this.$root = $root;
+    this.listeners = listeners;
   }
+
+  initDOMListeners() {
+    this.listeners.forEach(listener => {
+      const methods = getMethodName(listener);
+      console.log(' this.$root.on: ', this.$root);
+      /* Тоже самое, что и addEventListener */
+      this.$root.on(listener, this[methods]);
+    });
+  }
+
+  removeDOMListeners() {
+
+  }
+}
+
+/* input -> onInput */
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName);
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -2,8 +2,15 @@ import {DomListener} from '@core/DomListener';
 
 export class ExcelComponent extends DomListener {
   /* Возвращает шаблон компонента */
+  constructor($root, options = {}) {
+    super($root, options.listeners);
+  }
 
   toHTML() {
     return '';
+  }
+
+  init() {
+    this.initDOMListeners();
   }
 }
